937e0cde15ed54ae60c200d4d0f0d5bdd12946c6
y.hirano <yasuakihappy@gmail.com>
2020-07-31 23:45:36 +0900

    ユーザー登録機能作成

--- user/register.php
+++ user/register.php
@@ -1,24 +1,34 @@
 <?php
-$user_name = $_POST['user_name'];
-$user_email = $_POST['user_email'];
-$user_password = $_POST['user_password'];
+    // パラメータ取得
+    $user_name = $_POST['user_name'];
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
 
-$mysqli = mysqli_connect('localhost', 'root', 'password', 'simple_memo', '13306');
+    // TODO: バリデーション
 
-if (!$mysqli) {
-    echo "Error: Unable to connect to MySQL.<br>";
-    echo "Debugging errno: " . mysqli_connect_errno() . "<br>";
-    echo "Debugging error: " . mysqli_connect_error() . "<br>";
+    // DB接続処理
+    try
+    {
+        $database_handler = new PDO('mysql:host=db;dbname=simple_memo;charset=utf8mb4', 'root', 'password');
+    }
+    catch (PDOException $e)
+    {
+        echo "DB接続に失敗しました。<br />";
+        echo $e->getMessage();
+        exit;
+    }
+
+    // インサートSQLを作成して実行
+    if ($statement = $database_handler->prepare('INSERT INTO users (name, email, password) values(:name, :email, :password)'))
+    {
+        $password = password_hash($user_password, PASSWORD_DEFAULT);
+
+        $statement->bindParam(':name', $user_name);
+        $statement->bindParam(':email', $user_email);
+        $statement->bindParam(':password', $password);
+        $statement->execute();
+    }
+
+    // メモ投稿画面にリダイレクト
+    header('Location: ../memo/');
     exit;
-}
-
-if ($stmt = $mysqli->prepare('INSERT INTO users (name, email, password) values(?, ?, ?)')) {
-    $stmt->bind_param("s", $user_name);
-    $stmt->bind_param("s", $user_email);
-
-    $password = password_hash($user_password, PASSWORD_DEFAULT);
-    $stmt->bind_param("s", $password);
-    $stmt->execute();
-}
-
-mysqli_close($mysqli);
