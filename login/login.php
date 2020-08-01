112702eff59ca1b810f78f3826b533498f6bf9f4
y.hirano <yasuakihappy@gmail.com>
2020-08-01 23:41:53 +0900

    ログイン時パリデーションとログイン処理

---
+++ login/login.php
@@ -0,0 +1,49 @@
+<?php
+    session_start();
+    require '../common/validation.php';
+    require '../common/database.php';
+
+    // パラメータ取得
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
+
+    // バリデーション
+    $_SESSION['errors'] = [];
+
+    // - 空チェック
+    emptyCheck($_SESSION['errors'], $user_email, "メールアドレスを入力してください。");
+    emptyCheck($_SESSION['errors'], $user_password, "パスワードを入力してください。");
+
+    // - 文字数チェック
+    stringMaxSizeCheck($_SESSION['errors'], $user_email, "メールアドレスは255文字以内で入力してください。");
+    stringMaxSizeCheck($_SESSION['errors'], $user_password, "パスワードが空は255文字以内で入力してください。");
+
+    if($_SESSION['errors']) {
+        header('Location: ../login/');
+        exit;
+    }
+
+    // ログイン処理
+    $database_handler = getDatabaseConnection();
+    if ($statement = $database_handler->prepare('SELECT name, password FROM users WHERE email = :user_email')) {
+
+        $statement->bindParam(':user_email', $user_email);
+        $statement->execute();
+
+        $user = $statement->fetch(PDO::FETCH_ASSOC);
+        $name = $user['name'];
+
+        if (password_verify($user_password, $user['password'])) {
+            $_SESSION['user'] = [
+                'name' => $name,
+            ];
+            header('Location: ../memo/');
+            exit;
+        } else {
+            $_SESSION['errors'] = [
+                'メールアドレスまたはパスワードが間違っています。'
+            ];
+            header('Location: ../login/');
+            exit;
+        }
+    }
