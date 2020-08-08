651635ade02e079704ad3f9777b0905fa9375e40
y.hirano <yasuakihappy@gmail.com>
2020-08-08 21:52:11 +0900

    [1章] ユーザー登録画面作成

---
+++ common/header.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * タイトルを指定してヘッダーを作成する
+ * @param $title
+ * @return string
+ */
+function getHeader($title) {
+    return <<<EOF
+    <head>
+        <meta charset="utf-8" />
+        <title>SimpleMemo | {$title}</title>
+        <link rel="stylesheet" type="text/css" href="../public/css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="../public/css/main.css" />
+        <script defer src="../public/js/all.js"></script>
+    </head>
+
+EOF;
+}
