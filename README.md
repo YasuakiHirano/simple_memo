a1a9aed738fe6cdb9a856fa91795c740c97b0dc2
y.hirano <yasuakihappy@gmail.com>
2020-08-29 23:46:11 +0900

    [2章] 投稿機能作成

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 ## simple memo  
 phpのみで作成したEvernote風メモアプリです。  
 nginx1.19.1 + php7.4-fpm + mysql5.7を使用しています。  
+
+## 起動する  
+docker-composeを使って、下記のコマンドで起動できます。  
+
+```
+docker-compose -f .docker_memo/docker-compose.yml up -d
+```
+
+## データベースの設定  
+### データベース作成  
+http://localhost:8081 からphpmyadminを開いて、下記のsqlでデータベースを作成します。  
+
+```
+CREATE DATABASE simple_memo DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+```
+
+### テーブル作成  
+作成したデータベースを選択して、下記のsqlでテーブルを作成します。  
+
+```
+CREATE TABLE users (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+```
+
+```
+CREATE TABLE memos (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    user_id BIGINT NOT NULL,
+    title VARCHAR(255),
+    content TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+```
--- memo/index.php
+++ memo/index.php
@@ -1,3 +1,34 @@
+<?php
+    require '../common/auth.php';
+    require '../common/database.php';
+
+    if (!isLogin()) {
+        header('Location: ../login/');
+        exit;
+    }
+
+    $user_name = getLoginUserName();
+    $user_id = getLoginUserId();
+
+    $memos = [];
+    $database_handler = getDatabaseConnection();
+    if ($statement = $database_handler->prepare("SELECT id, title, content, updated_at FROM memos WHERE user_id = :user_id ORDER BY updated_at DESC")) {
+        $statement->bindParam(':user_id', $user_id);
+        $statement->execute();
+
+        while ($result = $statement->fetch(PDO::FETCH_ASSOC)) {
+           array_push($memos, $result);
+        }
+    }
+
+    $edit_id = "";
+    if (isset($_SESSION['select_memo'])) {
+        $edit_memo = $_SESSION['select_memo'];
+        $edit_id = empty($edit_memo['id']) ? "" : $edit_memo['id'];
+        $edit_title = empty($edit_memo['title']) ? "" : $edit_memo['title'];
+        $edit_content = empty($edit_memo['content']) ? "" : $edit_memo['content'];
+    }
+?>
 <!DOCTYPE html>
 <html lang="ja">                                                                                                                                                                    
     <?php
@@ -10,56 +41,57 @@
                 <div class="col-3 h-100 m-0 p-0 border-left border-right border-gray">
                     <div class="left-memo-menu d-flex justify-content-between pt-2">
                         <div class="pl-3 pt-2">
-                            xxxさん、こんにちは。
+                            <?php echo $user_name; ?>さん、こんにちは。
                         </div>
                         <div class="pr-1">
-                            <a href="" class="btn btn-success"><i class="fas fa-plus"></i></a>
-                            <a href="../login/" class="btn btn-dark"><i class="fas fa-sign-out-alt"></i></a>
+                            <a href="./action/add.php" class="btn btn-success"><i class="fas fa-plus"></i></a>
+                            <a href="./action/logout.php" class="btn btn-dark"><i class="fas fa-sign-out-alt"></i></a>
                         </div>
                     </div>
                     <div class="left-memo-title h3 pl-3 pt-3">
                         メモリスト
                     </div>
                     <div class="left-memo-list list-group-flush p-0">
-                        <a href="#" class="list-group-item list-group-item-action">
+                        <?php if(empty($memos)): ?>
+                            <div class="pl-3 pt-3 h5 text-info text-center">
+                                <i class="far fa-surprise"></i>メモがありません。
+                            </div>
+                        <?php endif; ?>
+                        <?php foreach($memos as $memo): ?>
+                        <a href="./action/select.php?id=<?php echo $memo['id']; ?>" class="list-group-item list-group-item-action <?php echo $edit_id == $memo['id'] ? 'active' : ''; ?>">
                             <div class="d-flex w-100 justify-content-between">
-                                <h5 class="mb-1">メモタイトル1</h5>
-                                <small>2020/08/01 09:00</small>
+                                <h5 class="mb-1"><?php echo $memo["title"] ?></h5>
+                                <small><?php echo date('Y/m/d H:i', strtotime($memo['updated_at'])); ?></small>
                             </div>
                             <p class="mb-1">
-                                メモ詳細1
+                                <?php
+                                    if (mb_strlen($memo['content']) <= 100) {
+                                        echo $memo['content'];
+                                    } else {
+                                        echo mb_substr($memo['content'], 0, 100) . "...";
+                                    }
+                                ?>
                             </p>
                         </a>
-                        <a href="#" class="list-group-item list-group-item-action">
-                            <div class="d-flex w-100 justify-content-between">
-                                <h5 class="mb-1">メモタイトル2</h5>
-                                <small>2020/08/01 09:00</small>
-                            </div>
-                            <p class="mb-1">
-                                メモ詳細2
-                            </p>
-                        </a>
-                        <a href="#" class="list-group-item list-group-item-action">
-                            <div class="d-flex w-100 justify-content-between">
-                                <h5 class="mb-1">メモタイトル3</h5>
-                                <small>2020/08/01 09:00</small>
-                            </div>
-                            <p class="mb-1">
-                                メモ詳細3
-                            </p>
-                        </a>
+                        <?php endforeach; ?>
                     </div>
                 </div>
                 <div class="col-9 h-100">
-                    <form class="w-100 h-100" method="post">
-                        <input type="hidden" name="edit_id" value="" />
-                        <div id="memo-menu">
-                            <button type="submit" class="btn btn-danger" formaction=""><i class="fas fa-trash-alt"></i></button>
-                            <button type="submit" class="btn btn-success" formaction=""><i class="fas fa-save"></i></button>
+                    <?php if(isset($_SESSION['select_memo'])): ?>
+                        <form class="w-100 h-100" method="post">
+                            <input type="hidden" name="edit_id" value="<?php echo $edit_id; ?>" />
+                            <div id="memo-menu">
+                                <button type="submit" class="btn btn-danger" formaction="./action/delete.php"><i class="fas fa-trash-alt"></i></button>
+                                <button type="submit" class="btn btn-success" formaction="./action/update.php"><i class="fas fa-save"></i></button>
+                            </div>
+                            <input type="text" id="memo-title" name="edit_title" placeholder="タイトルを入力する..." value="<?php echo $edit_title; ?>" />
+                            <textarea id="memo-content" name="edit_content" placeholder="内容を入力する..."><?php echo $edit_content; ?></textarea>
+                        </form>
+                    <?php else: ?>
+                        <div class="mt-3 alert alert-info">
+                            <i class="fas fa-info-circle"></i>メモを新規作成するか選択してください。
                         </div>
-                        <input type="text" id="memo-title" name="edit_title" placeholder="タイトルを入力する..." value="" />
-                        <textarea id="memo-content" name="edit_content" placeholder="内容を入力する..."></textarea>
-                    </form>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
